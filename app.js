47dba71eac54509addbe81c3fe352e0f56e7972d
VinayVidhehi <vinayvidhehi@gmail.com>
2023-11-26 19:13:43 +0530

    first backend for arduino

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const cors = require('cors');  // Import the cors module
+
+try {
+  mongoose.connect("mongodb+srv://Vinay:4556%40Devaraj@cluster0.tpgkfpg.mongodb.net/bat");
+  console.log("connected to mongodb");
+} catch(err) {
+  console.log(err.message);
+}
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());  // Enable CORS for all routes
+
+app.post('/microphone_file', async(req, res) => {
+  console.log(req.body);
+  res.status(200).send({message:"data received"});
+})
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
